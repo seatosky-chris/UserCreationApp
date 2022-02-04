a6e47f3174e7c10ad31ac2388733498310727612
Chris Jantzen <chris.m.jantzen@gmail.com>
2022-02-03 16:53:03 -0800

    Modified SoftwareSettings view to use the global config and setup saving.

--- UserCreationUI/ViewModels/GlobalSettings_SubViewModels/SoftwareSettingsViewModel.cs
+++ UserCreationUI/ViewModels/GlobalSettings_SubViewModels/SoftwareSettingsViewModel.cs
@@ -23,37 +23,14 @@ namespace UserCreationUI.GlobalSettings.ViewModels
 
         public SoftwareSettingsViewModel(IScreen screen) : base(screen)
         {
-            // TEST data
-            CurrentSoftware.Add(new SoftwareModel 
-            {
-                Id = "1",
-                Name = "Microsoft Project"
-            });
-            CurrentSoftware.Add(new SoftwareModel
-            {
-                Id = "2",
-                Name = "Microsoft Project Pro"
-            });
-            CurrentSoftware.Add(new SoftwareModel
-            {
-                Id = "3",
-                Name = "Spectrum"
-            });
-            CurrentSoftware.Add(new SoftwareModel
-            {
-                Id = "4",
-                Name = "Foundation 3000"
-            });
 
-            // Load data
-            LoadData();
         }
 
-        public ObservableCollection<SoftwareModel> CurrentSoftware { get; } = new();
+        public ObservableCollection<SoftwareModel> CurrentSoftware { get; } = new ObservableCollection<SoftwareModel>(Program.GlobalConfig.Software);
 
-        public ObservableCollection<O365GroupModel> O365Groups_All { get; } = new();
-        public ObservableCollection<ADPermissionModel> ADPermissions_All { get; } = new();
-        public ObservableCollection<O365LicenseModel> O365Licenses_All { get; } = new();
+        public ObservableCollection<O365GroupModel> O365Groups_All { get; } = new ObservableCollection<O365GroupModel>(Program.GlobalConfig.O365Groups);
+        public ObservableCollection<ADPermissionModel> ADPermissions_All { get; } = new ObservableCollection<ADPermissionModel>(Program.GlobalConfig.ADPermissions);
+        public ObservableCollection<O365LicenseModel> O365Licenses_All { get; } = new ObservableCollection<O365LicenseModel>(Program.GlobalConfig.O365Licenses);
         public ObservableCollection<O365GroupModel> O365Groups_Selected { get; } = new();
         public ObservableCollection<ADPermissionModel> ADPermissions_Selected { get; } = new();
         public ObservableCollection<O365LicenseModel> O365Licenses_Selected { get; } = new();
@@ -91,7 +68,7 @@ namespace UserCreationUI.GlobalSettings.ViewModels
             EditID = "";
         }
 
-        public void AddEditSoftware(string? Id)
+        private void AddEditSoftware(string? Id)
         {
             // TODO: Add in validation
             CurrentSoftware.Add(new SoftwareModel
@@ -107,7 +84,7 @@ namespace UserCreationUI.GlobalSettings.ViewModels
             Saveable = true;
         }
         
-        public void ClearForm()
+        private void ClearForm()
         {
             AddNewPrimary = "";
             O365Groups_Selected.Clear();
@@ -138,6 +115,8 @@ namespace UserCreationUI.GlobalSettings.ViewModels
         {
             if (Saveable)
             {
+                Program.GlobalConfig.Software = CurrentSoftware.ToList();
+
                 // Code to save to DB here
                 System.Diagnostics.Debug.WriteLine("Saving Software Settings");
                 HostScreen.Router.NavigateBack.Execute(Unit.Default);
@@ -187,187 +166,6 @@ namespace UserCreationUI.GlobalSettings.ViewModels
         {
             O365LicenseModel O365License = (O365LicenseModel)row.DataContext;
             O365Licenses_Selected.Add(O365License);
-        }
-
-        private void LoadData()
-        {
-            LoadADGroups();
-            LoadO365Groups();
-            LoadO365Licenses();
-        }
-
-        private void LoadADGroups()
-        {
-            // Loads fake test data AD groups
-            ADPermissions_All.Add(new ADPermissionModel
-            {
-                Guid = "72774d1f-3385-423e-ab59-4e42292fae0c",
-                Name = "10GB_UserFolder_Quota",
-                ADDescription = "Creates&maps a users folder and limits it to 10GB",
-                ITGType = "File Share / Drive Mappings",
-                ITGDescription = "Creates&maps a users folder and limits it to 10GB",
-                ITGWhoToAdd = "",
-                ITGApprover = "",
-                ITGLink = "https://seatosky.itglue.com/3136459/assets/174160-ad-security-groups/records/17927420"
-            });
-
-            ADPermissions_All.Add(new ADPermissionModel
-            {
-                Guid = "4bd6e9a9-2291-439e-b797-fc8de1818dcd",
-                Name = "AccountDetailRW",
-                ADDescription = "",
-                ITGType = "Other",
-                ITGDescription = "",
-                ITGWhoToAdd = "",
-                ITGApprover = "",
-                ITGLink = "https://seatosky.itglue.com/3136459/assets/174160-ad-security-groups/records/20343806"
-            });
-
-            ADPermissions_All.Add(new ADPermissionModel
-            {
-                Guid = "cdaa64b1-552f-4d8c-9e5d-6b638f016b7a",
-                Name = "B2W Addin Users",
-                ADDescription = "",
-                ITGType = "Applications",
-                ITGDescription = "",
-                ITGWhoToAdd = "",
-                ITGApprover = "",
-                ITGLink = "https://seatosky.itglue.com/3136459/assets/174160-ad-security-groups/records/17927454"
-            });
-
-            ADPermissions_All.Add(new ADPermissionModel
-            {
-                Guid = "4efd4fcd-ac8d-4deb-b71b-29c59f3b563b",
-                Name = "Cheque Printer Users",
-                ADDescription = "",
-                ITGType = "Printers",
-                ITGDescription = "This group appears to do nothing anymore. At least it is not tied to any printer's from what I can tell.",
-                ITGWhoToAdd = "",
-                ITGApprover = "",
-                ITGLink = "https://seatosky.itglue.com/3136459/assets/174160-ad-security-groups/records/17925832"
-            });
-        }
-
-        private void LoadO365Groups()
-        {
-            // Loads fake test data O365 groups
-            O365Groups_All.Add(new O365GroupModel
-            {
-                ObjectID = "ef24c6fc-5046-4c28-8efe-aa0af0907133",
-                Name = "NLL Employees Hourly",
-                Email = "All-HourlyEmployees@norlandlimited.com",
-                GroupType = O365GroupType.DL,
-                O365Description = "",
-                ITGDescription = "All employee's who are paid by the hour. This group is updated automatically by a PowerShell script.",
-                ITGWhoToAdd = "Employee's who are paid by the hour",
-                ITGApprover = "",
-                ITGLink = "https://seatosky.itglue.com/3136459/assets/197503-email-groups/records/29950472"
-            });
-
-            O365Groups_All.Add(new O365GroupModel
-            {
-                ObjectID = "844a4b80-7acf-4d46-80e1-fa97a2e9f380",
-                Name = "NLL Employees Salary",
-                Email = "All-SalariedEmployees@norlandlimited.com",
-                GroupType = O365GroupType.DL,
-                O365Description = "",
-                ITGDescription = "All employee's who are on salary. This group is updated automatically by a PowerShell script.",
-                ITGWhoToAdd = "Employee's who are paid on a salary",
-                ITGApprover = "",
-                ITGLink = "https://seatosky.itglue.com/3136459/assets/197503-email-groups/records/29950475"
-            });
-
-            O365Groups_All.Add(new O365GroupModel
-            {
-                ObjectID = "f25f774c-e8f9-4ddf-ba1d-3135bd43524e",
-                Name = "Pinnacle Accounting & Finance",
-                Email = "PinnacleAccountingFinance@norlandlimited.com",
-                GroupType = O365GroupType.M365Group,
-                O365Description = "",
-                ITGDescription = "Pinnacle Accounting & Finance",
-                ITGWhoToAdd = "",
-                ITGApprover = "",
-                ITGLink = "https://seatosky.itglue.com/3136459/assets/197503-email-groups/records/36778356"
-            });
-
-            O365Groups_All.Add(new O365GroupModel
-            {
-                ObjectID = "6e2f18ab-d7d6-40c8-8390-1d60d1166204",
-                Name = "Spectrum Users",
-                Email = "Spectrum-Users@norlandlimited.com",
-                GroupType = O365GroupType.DL,
-                O365Description = "",
-                ITGDescription = "Sends email to all users of Spectrum.",
-                ITGWhoToAdd = "Any users with a spectrum account.",
-                ITGApprover = "",
-                ITGLink = "https://seatosky.itglue.com/3136459/assets/197503-email-groups/records/17916087"
-            });
-
-            O365Groups_All.Add(new O365GroupModel
-            {
-                ObjectID = "e674d876-249b-4ab3-a2df-db951611c786",
-                Name = "AP - High Standard",
-                Email = "ap@highstandard.ca",
-                GroupType = O365GroupType.SharedMailbox,
-                O365Description = "",
-                ITGDescription = "",
-                ITGWhoToAdd = "",
-                ITGApprover = "",
-                ITGLink = "https://seatosky.itglue.com/3136459/assets/197503-email-groups/records/36779171"
-            });
-
-            O365Groups_All.Add(new O365GroupModel
-            {
-                ObjectID = "a2fd9dca-bbb2-4853-9533-fd5830b1d91e",
-                Name = "B2WSupport",
-                Email = "b2wsupport@norlandlimited.com",
-                GroupType = O365GroupType.SharedMailbox,
-                O365Description = "",
-                ITGDescription = "",
-                ITGWhoToAdd = "",
-                ITGApprover = "",
-                ITGLink = "https://seatosky.itglue.com/3136459/assets/197503-email-groups/records/36779129"
-            });
-        }
-
-        private void LoadO365Licenses()
-        {
-            // Load fake test data O365 Licenses
-            O365Licenses_All.Add(new O365LicenseModel
-            {
-                IDName = "AAD_PREMIUM",
-                Name = "AZURE ACTIVE DIRECTORY PREMIUM P1"
-            });
-
-            O365Licenses_All.Add(new O365LicenseModel
-            {
-                IDName = "SMB_APPS",
-                Name = "Business Apps (free)"
-            });
-
-            O365Licenses_All.Add(new O365LicenseModel
-            {
-                IDName = "EXCHANGESTANDARD",
-                Name = "Exchange Online (Plan 1)"
-            });
-
-            O365Licenses_All.Add(new O365LicenseModel
-            {
-                IDName = "O365_BUSINESS",
-                Name = "MICROSOFT 365 APPS FOR BUSINESS"
-            });
-
-            O365Licenses_All.Add(new O365LicenseModel
-            {
-                IDName = "O365_BUSINESS_PREMIUM",
-                Name = "MICROSOFT 365 BUSINESS STANDARD"
-            });
-
-            O365Licenses_All.Add(new O365LicenseModel
-            {
-                IDName = "TEAMS_FREE",
-                Name = "MICROSOFT TEAMS (FREE)"
-            });
         }
 
         public void OpenUrl(string url)
