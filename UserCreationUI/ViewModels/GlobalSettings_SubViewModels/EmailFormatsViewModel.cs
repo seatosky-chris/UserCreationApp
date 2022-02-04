f9fde3f6cc3c19bd7dc6b63c60173b3662ab6ae7
Chris Jantzen <chris.m.jantzen@gmail.com>
2022-02-03 16:41:43 -0800

    Updated email formats view to use global config and added edit functionality.

--- UserCreationUI/ViewModels/GlobalSettings_SubViewModels/EmailFormatsViewModel.cs
+++ UserCreationUI/ViewModels/GlobalSettings_SubViewModels/EmailFormatsViewModel.cs
@@ -1,7 +1,11 @@
-﻿using ReactiveUI;
+﻿using Avalonia.Controls;
+using ReactiveUI;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive;
+using UserCreationLibrary;
+using UserCreationUI.Models.ExtendedModels;
 
 namespace UserCreationUI.GlobalSettings.ViewModels
 {
@@ -10,60 +14,70 @@ namespace UserCreationUI.GlobalSettings.ViewModels
         // Unique identifier for the routable view model.
         public new string UrlPathSegment { get; } = "EmailFormatsEdit";
 
-        private List<string> _domains = new List<string>();
+        private List<string> _domains = Program.GlobalConfig.EmailDomains;
         private int _selectedDomain = 0;
+        private string _editID = "";
 
         public EmailFormatsViewModel(IScreen screen) : base(screen)
         {
-            // TEST data
-            CurrentFormats.Add("[First].[Last]@canmine.ca (Location: Canmine Contracting)");
-            CurrentFormats.Add("[First].[Last]@highstandard.ca (Location: High Standard Scaffolding)");
-            CurrentFormats.Add("[First].[Last]@pinnacledrilling.ca (Location: Pinnacle Drilling)");
-            CurrentFormats.Add("[First].[Last]@traxxon.ca (Location: Traxxon Rock Drills)");
 
-            // Load domains
-            Domains.Add("canmine.ca");
-            Domains.Add("highstandard.ca");
-            Domains.Add("norlandlimited.com");
-            Domains.Add("pinnacledrilling.ca");
-            Domains.Add("traxxon.com");
         }
 
-        public ObservableCollection<string> CurrentFormats { get; } = new();
+        public ObservableCollection<EmailDefaultModelExtended> CurrentFormats { get; } = new ObservableCollection<EmailDefaultModelExtended>(Program.GlobalConfig.EmailFormats);
+
+        public string EditID
+        {
+            get => _editID;
+            set => this.RaiseAndSetIfChanged(ref _editID, value);
+        }
 
         public void AddFormat()
         {
+            AddEditFormat(null);
+        }
+
+        public void EditFormat()
+        {
+            EmailDefaultModelExtended CurrentFormatItem = CurrentFormats.Where(x => x.Id == EditID).FirstOrDefault();
+
+            AddEditFormat(CurrentFormatItem.Id);
+
+            CurrentFormats.Remove(CurrentFormats.Where(x => x.Id == EditID).FirstOrDefault());
+            EditID = "";
+        }
+
+        public void CancelEditFormat()
+        {
+            ClearForm();
+            EditID = "";
+        }
+
+        private void AddEditFormat(string? Id)
+        {
+            // TODO: Add in validation
             // TODO: Add in check to ensure this is a proper format
             // also only enable the button if AddNewPrimary is a proper format
 
-            string Domain = Domains[SelectedDomain];
-            var LocationIndexes = SelectedLocations.SelectedIndexes;
-            var EmployeeTypeIndexes = SelectedEmployeeTypes.SelectedIndexes;
-            string NewFormatString = $"{AddNewPrimary}@{Domain} (";
-
-            if (LocationIndexes.Count > 0)
+            CurrentFormats.Add(new EmailDefaultModelExtended
             {
-                NewFormatString += "Locations:";
-                foreach (var i in LocationIndexes)
-                {
-                    NewFormatString += $" {Locations[i]}";
-                }
-            }
+                Id = Id ?? System.Guid.NewGuid().ToString(),
+                Priority = 1,
+                EmailFormat = AddNewPrimary,
+                Domain = Domains[SelectedDomain],
+                EmployeeTypes = (from employee in SelectedEmployeeTypes.SelectedItems select employee.Key).Distinct().ToList(),
+                Locations = (from location in SelectedLocations.SelectedItems select location.Key).Distinct().ToList()
+            });
 
-            if (EmployeeTypeIndexes.Count > 0)
-            {
-                NewFormatString += "Employee Types:";
-                foreach (var i in EmployeeTypeIndexes)
-                {
-                    NewFormatString += $" {EmployeeTypes[i]}";
-                }
-            }
-
-            NewFormatString += ")";
-
-            CurrentFormats.Add(NewFormatString);
-            AddNewPrimary = "";
+            ClearForm();
             Saveable = true;
+        }
+
+        private void ClearForm()
+        {
+            AddNewPrimary = "";
+            SelectedDomain = 0;
+            SelectedLocations.Clear();
+            SelectedEmployeeTypes.Clear();
         }
 
         public void DeleteFormat(int index)
@@ -76,9 +90,33 @@ namespace UserCreationUI.GlobalSettings.ViewModels
         {
             if (Saveable)
             {
+                Program.GlobalConfig.EmailFormats = CurrentFormats.ToList();
+
                 // Code to save to DB here
                 System.Diagnostics.Debug.WriteLine("Saving Email Format Settings");
                 HostScreen.Router.NavigateBack.Execute(Unit.Default);
+            }
+        }
+
+        public void CurrentFormat_DoubleClick(ListBoxItem row)
+        {
+            // Load format details
+            EmailDefaultModelExtended CurrentFormat = (EmailDefaultModelExtended)row.DataContext;
+            EditID = CurrentFormat.Id;
+
+            ClearForm();
+
+            AddNewPrimary = CurrentFormat.EmailFormat;
+            SelectedDomain = Domains.IndexOf(CurrentFormat.Domain);
+
+            foreach (var TypeIndex in CurrentFormat.EmployeeTypes)
+            {
+                SelectedEmployeeTypes.Select(TypeIndex);
+            }
+
+            foreach (var LocIndex in CurrentFormat.Locations)
+            {
+                SelectedLocations.Select(LocIndex);
             }
         }
 
